701d567bafa004fcf30c7e6016fd1574e73bd7af
Star1xr <166748405+Star1xr@users.noreply.github.com>
2026-05-06 20:55:35 +0300

    Update build.gradle.kts

--- ZalithLauncher/build.gradle.kts
+++ ZalithLauncher/build.gradle.kts
@@ -55,9 +55,9 @@ android {
     signingConfigs {
         create("releaseBuild") {
 	        storeFile = file("zalith_launcher.jks")
-            storePassword = System.getenv("STORE_PASSWORD") ?: getKeyFromLocal("STORE_PASSWORD", ".store_password.txt")
+            storePassword = getKeyFromLocal("STORE_PASSWORD", ".store_password.txt")
             keyAlias = "zalithkey"
-            keyPassword = System.getenv("KEY_PASSWORD") ?: getKeyFromLocal("KEY_PASSWORD", ".key_password.txt")
+            keyPassword = getKeyFromLocal("KEY_PASSWORD", ".key_password.txt")
         }
         create("debugBuild") {
             storeFile = file("zalith_launcher_debug.jks")
