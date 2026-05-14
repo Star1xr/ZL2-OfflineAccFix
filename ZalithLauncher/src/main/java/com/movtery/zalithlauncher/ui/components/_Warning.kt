5f6ee0ddaeec9ee0af1c61c507e4123abd399a78
Star1xr <star1xx77ff@gmail.com>
2026-05-14 14:36:31 +0300

    fix imports _Warning.kt

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/components/_Warning.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/components/_Warning.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -37,6 +38,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.movtery.zalithlauncher.R
